c38ac2b7d5d13163fb290e6285b03e9b24b1a997
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2023-11-17 13:49:06 +0200

    week calendar impl

--- app/src/main/java/com/example/todoer/MainActivity.kt
+++ app/src/main/java/com/example/todoer/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todoer.ui.home.HomeScreen
+import com.example.todoer.ui.home.HomeState
 import com.example.todoer.ui.theme.TodoerTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeScreen()
+                    HomeScreen(HomeState(1))
                 }
             }
         }
--- app/src/main/java/com/example/todoer/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/todoer/ui/home/HomeScreen.kt
@@ -1,20 +1,49 @@
 package com.example.todoer.ui.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.todoer.R
 import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.WeekCalendar
 import com.kizitonwose.calendar.compose.weekcalendar.rememberWeekCalendarState
+
 import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.Locale
 
 @Composable
-fun HomeScreen() {
-
+fun HomeScreen(state: HomeState) {
+    //TODO: insert variables in state
     val startDay = LocalDate.now()
     val endDay = startDay.plusDays(7)
-
+    var selection by remember { mutableStateOf(startDay) }
 
     val weekCalendarState = rememberWeekCalendarState(
         startDate = startDay,
@@ -22,9 +51,72 @@ fun HomeScreen() {
         firstDayOfWeek = DayOfWeek.MONDAY,
     )
 
-    WeekCalendar (
-        state = weekCalendarState,
-        dayContent = { day -> Text(text = day.date.dayOfWeek.name) },
-        modifier = Modifier,
-    )
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+    ) {
+        WeekCalendar (
+            modifier = Modifier,
+            state = weekCalendarState,
+            dayContent = { day ->
+                Day(day.date, isSelected = selection == day.date) { clicked ->
+                    if (selection != clicked) {
+                        selection = clicked
+                    }
+                }
+            }
+        )
+    }
+}
+
+private val dateFormatter = DateTimeFormatter.ofPattern("dd")
+
+@Composable
+private fun Day(date: LocalDate, isSelected: Boolean, onClick: (LocalDate) -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable { onClick(date) },
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            modifier = Modifier.padding(vertical = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+        ) {
+            Text(
+                text = date.dayOfWeek.getDisplayName(
+                    TextStyle.SHORT,
+                    LocalConfiguration.current.locales.get(0)
+                ),
+                fontSize = 12.sp,
+                color = if (isSelected) Color.Blue else Color.Black,
+                fontWeight = FontWeight.Light,
+            )
+            Text(
+                text = dateFormatter.format(date),
+                fontSize = 14.sp,
+                color = if (isSelected) Color.Blue else Color.Black,
+                fontWeight = FontWeight.Bold,
+            )
+        }
+        if (isSelected) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(5.dp)
+                    .background(Color.Blue)
+                    .align(Alignment.BottomCenter),
+            )
+        }
+    }
+}
+
+
+@Composable
+@Preview
+fun HomeScreenPreview() {
+    HomeScreen(state = HomeState(1))
 }
