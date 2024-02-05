0783f068fbbf3e87ad1215ce1e6adb7e32a16e63
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2024-02-05 17:35:32 +0200

    WeekCalendar docs:https://github.com/kizitonwose/Calendar/blob/main/docs/Compose.md#usage

--- app/src/main/java/com/example/todoer/TodoerApp.kt
+++ app/src/main/java/com/example/todoer/TodoerApp.kt
@@ -1,8 +1,51 @@
 package com.example.todoer
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.kizitonwose.calendar.compose.HorizontalCalendar
+import com.kizitonwose.calendar.compose.WeekCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.compose.weekcalendar.rememberWeekCalendarState
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.WeekDay
+import com.kizitonwose.calendar.core.atStartOfMonth
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import java.time.LocalDate
+import java.time.YearMonth
 
 @Composable
 fun TodoerApp() {
+    val currentDate = remember { LocalDate.now() }
+    val currentMonth = remember { YearMonth.now() }
+    val startDate = remember { currentMonth.minusMonths(100).atStartOfMonth() } // Adjust as needed
+    val endDate = remember { currentMonth.plusMonths(100).atEndOfMonth() } // Adjust as needed
+    val firstDayOfWeek = remember { firstDayOfWeekFromLocale() } // Available from the library
 
+    val state = rememberWeekCalendarState(
+        startDate = startDate,
+        endDate = endDate,
+        firstVisibleWeekDate = currentDate,
+        firstDayOfWeek = firstDayOfWeek
+    )
+
+    WeekCalendar(
+        state = state,
+        dayContent = { Day(it) }
+    )
+}
+
+@Composable
+fun Day(day: WeekDay) {
+    Box(
+        modifier = Modifier
+            .aspectRatio(1f), // This is important for square sizing!
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = day.date.dayOfMonth.toString())
+    }
 }
